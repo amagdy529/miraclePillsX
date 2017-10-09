ad4e67fa07181861d11bbe6e13663fda1534d87b
amagdy529 <amagdy529@gmail.com>
2017-10-09 16:42:17 -0700

    edited in te controller .. functions for datasource for UIPicker

--- miraclePillsX/miraclePillsX/ViewController.swift
+++ miraclePillsX/miraclePillsX/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIPickerViewDataSource	, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["New York" , "Cairo" , "Dubai" , "london"  , "California"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = 	self
+        statePicker.delegate = self
 //        self.view.backgroundColor = UIColor.purple
         
     }
@@ -21,6 +29,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+      
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal ) //UIControlState()
+        
+        statePicker.isHidden = true
+    }
 
 }
 
